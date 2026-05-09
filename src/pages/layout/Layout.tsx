f54132de7e573f6b56050a2e94e331f293e4c8a2
Livelo Julio B. <julioeduardolivelo@gmail.com>
2026-05-09 12:00:00 +0800

    added navbar toggle on desktop

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,5 +1,5 @@
 import Navigation from "./components/Navigation";
-import { AppShell, Burger } from "@mantine/core";
+import { AppShell, Burger, Flex } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 interface LayoutProps {
   children: React.ReactNode;
@@ -14,13 +14,14 @@ function Layout({ children }: LayoutProps) {
       navbar={{
         width: 300,
         breakpoint: "sm",
-        collapsed: { mobile: !opened },
+        collapsed: { desktop: !opened, mobile: !opened },
       }}
     >
       <AppShell.Header>
-        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-
-        <div>Logo</div>
+        <Flex p="5" gap="10" align="center" h="100%">
+          <Burger opened={opened} onClick={toggle} size="sm" />
+          <div>Logo</div>
+        </Flex>
       </AppShell.Header>
 
       <AppShell.Navbar>
